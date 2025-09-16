794c34fa126e10d7f4e4602bc5ec23b953224046
xAgesx <thamer.douss@gmail.com>
2025-09-16 11:37:34 +0100

    Fixed Spawn rates and rarity

--- Assets/Scripts/ItemSpawner.cs
+++ Assets/Scripts/ItemSpawner.cs
@@ -14,11 +14,21 @@ public class ItemSpawner : MonoBehaviour {
     }
 
     public void startSpawning() {
-        InvokeRepeating("spawnItem", 0, 2);
+        InvokeRepeating("spawnItem", 0, 3);
     }
     void spawnItem() {
         //Debug.Log(candies.Count);
-        GameObject itemToSpawn = candies[Random.Range(0, candies.Count)];
+        //Choosing a random candy based on it's rarety 
+        int indexToSpawn = 0;
+        int i = Random.Range(0, 100);
+        switch (i) {
+            case int n when (n<40) : indexToSpawn = 0;break;
+            case int n when (n<70) : indexToSpawn = 1;break;
+            case int n when (n<90) : indexToSpawn = 2;break;
+            case int n when (n<=100) : indexToSpawn = 3;break;
+        }
+        Debug.Log(indexToSpawn);
+        GameObject itemToSpawn = candies[indexToSpawn];
 
         Vector2 pos = new Vector2(Random.Range(-2, 2), 4);
         Instantiate(itemToSpawn, pos, Quaternion.identity);
